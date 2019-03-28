5a135b81eb54313bd3819ad0229b2f0218551a31
diabagate <47230250+diabagate@users.noreply.github.com>
2019-03-28 18:44:28 +0000

    Add files via upload

---
+++ monprojet/index.js
@@ -0,0 +1,29 @@
+
+
+function titi() {
+	var x = document.getElementById('im1');
+	x.src="placali.jpg";
+}
+function tata(){
+	var y = document.getElementById('bjr');
+	y.src="bien-être.jpg";
+}
+function toto(){
+	var z = document.getElementById('im1');
+	z.src="boulet.jpg";
+}
+function tete(){
+	var w = document.getElementById('bjr');
+	w.src="sante.jpg";
+}
+function papa(){
+		var joli = document.getElementsById('res');
+		joli.src ="point.jpg";
+	}
+	function mama(){
+		var kalo = document.getElementById('res');
+		kalo.src ="examens.jpg";
+	}
+
+
+
